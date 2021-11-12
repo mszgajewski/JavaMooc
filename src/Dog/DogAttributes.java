84fd3619cc31b1b438b65297e0fa5e8bd70ddaef
mszgajewski <mszgajewski@gmail.com>
2021-11-12 21:43:54 +0100

    Mooc

---
+++ src/Dog/DogAttributes.java
@@ -0,0 +1,18 @@
+package Dog;
+
+import java.util.Scanner;
+
+public class DogAttributes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
+
+class Dog {
+    private String name;
+    private String breed;
+    private int age;
+
+}
