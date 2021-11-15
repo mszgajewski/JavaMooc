11a53d0513c5c1ec6bc9646d0abf6902e29b4b9e
mszgajewski <mszgajewski@gmail.com>
2021-11-15 21:39:17 +0100

    Dog

--- src/Dog/DogAttributes.java
+++ src/Dog/DogAttributes.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class DogAttributes {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        Dog Boo = new Dog("Boo", "terier", 12);
+
+        System.out.println(Boo);
 
     }
 }
@@ -15,4 +17,14 @@ class Dog {
     private String breed;
     private int age;
 
+    public Dog(String name,String breed, int age){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", " + this.breed+ ", "  + this.age;
+    }
 }
